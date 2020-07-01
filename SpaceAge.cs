1ef5cc0be7a8fd33b1b279787bf758a249621a98
Morgan Santamoor <msantamoor@gmail.com>
2020-06-30 20:36:13 -0600

    Cleaned Slightly

--- SpaceAge.cs
+++ SpaceAge.cs
@@ -6,49 +6,49 @@ public class SpaceAge
 
     public SpaceAge(double seconds)
     {
-        EYear = seconds / 31557600;
+        E = seconds / 31557600;
     }
 
-    public double EYear
+    public double E
     { get; }
 
     public double OnEarth()
     {
-        return EYear;
+        return E;
     }
 
     public double OnMercury()
     {
-        return EYear / .2408467;
+        return E / .2408467;
     }
 
     public double OnVenus()
     {
-        return EYear / .61519726;
+        return E / .61519726;
     }
 
     public double OnMars()
     {
-        return EYear / 1.8808158;
+        return E / 1.8808158;
     }
 
     public double OnJupiter()
     {
-        return EYear / 11.862615;
+        return E / 11.862615;
     }
 
     public double OnSaturn()
     {
-        return EYear / 29.447498;
+        return E / 29.447498;
     }
 
     public double OnUranus()
     {
-        return EYear / 84.016846;
+        return E / 84.016846;
     }
 
     public double OnNeptune()
     {
-        return EYear / 164.79132;
+        return E / 164.79132;
     }
 }
